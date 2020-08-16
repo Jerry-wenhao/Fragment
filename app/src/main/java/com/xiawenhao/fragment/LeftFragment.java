ea46264bad600fc9cf027b07e5f6975a56e469fe
Jerry777777 <491818175@qq.com>
2020-08-16 16:35:09 +0800

    [xiawenhao] feat: bind leftFragment with left two buttons

---
+++ app/src/main/java/com/xiawenhao/fragment/LeftFragment.java
@@ -0,0 +1,25 @@
+package com.xiawenhao.fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LeftFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.left_fragment, container, false);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+}
