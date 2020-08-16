6cda76d34637b58d56a07605b777c856a417ea0f
Jerry777777 <491818175@qq.com>
2020-08-16 19:26:00 +0800

    [xiawenhao] feat: set number by dimens

--- app/src/main/java/com/xiawenhao/fragment/MainActivity.java
+++ app/src/main/java/com/xiawenhao/fragment/MainActivity.java
@@ -1,6 +1,8 @@
 package com.xiawenhao.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
--- app/src/main/java/com/xiawenhao/fragment/RightFragmentOfJava.java
+++ app/src/main/java/com/xiawenhao/fragment/RightFragmentOfJava.java
@@ -8,10 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RightFragmentOfAndroid extends Fragment {
+public class RightFragmentOfJava extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.right_fragment_android, container, false);
+        return inflater.inflate(R.layout.right_fragment_java, container, false);
     }
 }
