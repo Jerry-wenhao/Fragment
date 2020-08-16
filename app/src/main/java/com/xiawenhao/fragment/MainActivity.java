ba61bd59485a7a84aa7b5c13027b5bcb5311e132
Jerry777777 <491818175@qq.com>
2020-08-16 20:45:15 +0800

    [xiawenhao] feat: add click button when click android and java button

--- app/src/main/java/com/xiawenhao/fragment/MainActivity.java
+++ app/src/main/java/com/xiawenhao/fragment/MainActivity.java
@@ -2,9 +2,14 @@ package com.xiawenhao.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.widget.Button;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,8 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
 
+    @OnClick({R.id.android_button, R.id.java_button})
+    public void onClick(Button button) {
+        switch (button.getId()) {
+            case R.id.android_button:
+                replaceFragment(new RightFragmentOfAndroid());
+                break;
+            case R.id.java_button:
+                replaceFragment(new RightFragmentOfJava());
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.right_fragment, fragment).commit();
+    }
 
     }
 
-}
